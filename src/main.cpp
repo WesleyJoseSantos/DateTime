ebe3d2d60e440509f6ee4645cebb0d35e93c91e9
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-05-10 04:18:17 -0300

    feat: implementation of test

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,45 @@
 #include <Arduino.h>
-#include "DateTime.hpp"
+#include "TimeClock.hpp"
 
-DateTime tc = DateTime(Y2K_TIMESTAMP);
+TimeClock clock1 = TimeClock(3, 15, 36);
+TimeClock clock2 = TimeClock(3, 16, 32);
+TimeClock clock3 = TimeClock(1, 17, 50);
+TimeClock clock4 = TimeClock(5, 20, 12);
+TimeClock clock5 = TimeClock(5, 20, 13);
+TimeClock clock6 = TimeClock(5, 20, 12);
 
 void setup() {
   // put your setup code here, to run once:
-
   Serial.begin(9600);
+}
+
+void testCompare(TimeClock ck1, TimeClock ck2){
+  Serial.print(ck1.toString());
+  Serial.print(" < ");
+  Serial.print(ck2.toString());
+  Serial.print(" = ");
+  Serial.println(ck1 < ck2);
+
+  Serial.print(ck1.toString());
+  Serial.print(" > ");
+  Serial.print(ck2.toString());
+  Serial.print(" = ");
+  Serial.println(ck1 > ck2);
+
+  Serial.print(ck1.toString());
+  Serial.print(" == ");
+  Serial.print(ck2.toString());
+  Serial.print(" = ");
+  Serial.println(ck1 == ck2);
+
+  Serial.println();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println(tc.toString());
+  testCompare(clock1, clock2);
+  testCompare(clock3, clock4);
+  testCompare(clock5, clock6);
+  testCompare(clock1, clock1);
   delay(1000);
 }
