138d66e183f510af3b00abde99de9ee10c4fdeb3
WESLEY JOSE SANTOS <53940899+WesleyJoseSantos@users.noreply.github.com>
2021-05-12 17:15:34 -0300

    feat: added module to monitor variables

---
+++ src/monitor.h
@@ -0,0 +1,17 @@
+/**
+ * @file monitor.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2021-05-11
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#ifndef __MONITOR__H__
+#define __MONITOR__H__
+
+#define monitor(x) Serial.print(#x);Serial.print(" = ");Serial.println(x);
+
+#endif  //!__MONITOR__H__
